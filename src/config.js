94d5810289c7eca3caeadef1c4207753b18bfc91
Ayush Shekhar <cadet4049@gmail.com>
2021-03-13 16:51:10 +0530

    changed config

--- src/config.js
+++ src/config.js
@@ -1,6 +1,6 @@
 // export const MASTER_CS_CONCEPT_API_URL = window._env_
 //   ? window._env_.IMAGE_PATH
 //   : process.env.IMAGE_PATH;
-// export const MASTER_CS_CONCEPT_API_URL =
-//   "https://mastercsconcepts.herokuapp.com/";
-export const MASTER_CS_CONCEPT_API_URL = "http://localhost:5000";
+export const MASTER_CS_CONCEPT_API_URL =
+  "https://mastercsconcepts.herokuapp.com/";
+// export const MASTER_CS_CONCEPT_API_URL = "http://localhost:5000";
